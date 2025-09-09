1aab5218fbcfa23f45cecfa57299c7fe6ac14700
srushtikulkarni <srushtikulkarni09@gmail.com>
2025-09-09 11:43:35 +0530

    adding functions for books

--- component/Book.jsx
+++ component/Book.jsx
@@ -1,6 +1,8 @@
+import { useState } from "react";
 import useFetch from "../useFetch";
 
 const Book = () =>{
+    const [successMessage, setSuccessMessage] = useState("");
     const {data, loading,error} = useFetch("https://backend-books-beta.vercel.app/books");
     console.log(data);
 
@@ -14,16 +16,38 @@ const Book = () =>{
     const shoeDog = books.find((b)=> b.title === "Shoe Dog");
 
     // books by J.K. Rowling
-    const bookByAuthor = books.find((b)=> b.author === 'J.K. Rowling')
-
+    const bookByAuthor = books.find((b)=> b.author === 'J.K. Rowling');
 
     console.log(books)
+
+    const handleDeleteBook = async (bookId) =>{
+        try{
+            const response = await fetch (`https://backend-books-beta.vercel.app/books/${bookId}`,
+
+                {
+                    method: "DELETE"
+                },
+            )
+            if(!response.ok){
+                throw "Failed to delete book."
+            }
+            const data = await response.json();
+            if(data){
+             setSuccessMessage("Book deleted successfully!");
+             window.location.reload();
+            }
+
+        }catch(error){
+            onslotchange.log(error);
+        }
+    }
+
     return(
         <div>
             <h1>All Books</h1>
             <ul>
                 {books.map((book)=>(
-                    <li>{book.title}</li>
+                    <li>{book.title}<button onClick={()=>handleDeleteBook(book._id)}>Delete</button></li>
                 ))}
             </ul>
         {shoeDog && (
